8f67bb9e7114b96b2600e1aeae9e243f3e9c97c5
lea-ha <lea.haidamous@lau.edu>
2023-09-30 20:06:42 +0300

    edited forloops.py

--- forloops.py
+++ forloops.py
@@ -1,17 +1,29 @@
 
-fruitsList = ("apple", "banana", "cherry", "watermelon")
+fruitsList = ("apple", "banana", "cherry", "watermelon", "pear", "avocado")
 
 #2 ways to iterate over an iterable
 for x in fruitsList: #Creating using an iterable
-    print(x)
+    print("using iterable: ", x)
 
 
 for x in range(len(fruitsList)): #Creating using the range function
-    print(fruitsList[x])
+    print("using[]: ", fruitsList[x])
 
 #Illustrating overloaded method range and the else in for loops
 for x in range(2,4):
     print(fruitsList[x])
 else:
-    print("The for loop finished!\n")
+    print("The for loop finished!\n\n\n")
+
+#Showing the break and continue statement
+#I want to buy only apple and cherry from the list:
+for x in fruitsList:
+    #I dont want bananas
+    if(x == "banana"): 
+        continue
+    #everything from watermelon and beyond I dont need.
+    if(x == "watermelon"):
+        break
+    print(x)
+
 
